107ac94463e482bacf3d0d467afbe4096b5e1cd9
saipradeep7932 <saipradeep7931@gmail.com>
2026-01-04 19:37:59 +0530

    feat(domain): add domain layer barrel exports

---
+++ backend/src/core/domain/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Domain layer barrel export
+ * Exports all domain entities, value objects, and constants
+ */
+export { Route } from "./Route";
+export * from "./value-objects";
+export * from "./constants";
+
---
+++ backend/src/core/domain/value-objects/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Value objects barrel export
+ * Exports all domain value objects for convenient importing
+ */
+export { RouteId } from "./RouteId";
+export { Year } from "./Year";
+export { VesselType, VesselTypeEnum } from "./VesselType";
+export { FuelType, FuelTypeEnum } from "./FuelType";
+export { Distance } from "./Distance";
+export { TotalEmissions } from "./TotalEmissions";
+export { FuelConsumption } from "./FuelConsumption";
+export { GHGIntensity } from "./GHGIntensity";
+export { ComplianceBalance } from "./ComplianceBalance";
+
